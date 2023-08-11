946e27dd27c30466ff540c6964f91815c53aaa18
samypr100 <3933065+samypr100@users.noreply.github.com>
2023-08-10 20:43:43 -0400

    Module Fixes fix: Additional Opens in module-info.java fix: Export-Package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,9 +12,13 @@ module eu.hansolo.toolboxfx {
     requires transitive eu.hansolo.toolbox;
 
     opens eu.hansolo.toolboxfx to eu.hansolo.toolbox;
+    opens eu.hansolo.toolboxfx.evt.type to eu.hansolo.toolbox;
+    opens eu.hansolo.toolboxfx.font to eu.hansolo.toolbox;
+    opens eu.hansolo.toolboxfx.geom to eu.hansolo.toolbox;
 
-    exports eu.hansolo.toolboxfx.font;
-    exports eu.hansolo.toolboxfx.evt.type;
-    exports eu.hansolo.toolboxfx.geom;
     exports eu.hansolo.toolboxfx;
+    exports eu.hansolo.toolboxfx.evt.type;
+    exports eu.hansolo.toolboxfx.font;
+    exports eu.hansolo.toolboxfx.geom;
+
 }
